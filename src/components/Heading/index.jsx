6965810a907ffa483d831002acab7a694e05ca8a
Fernando Rodrigues de Santana <fernando@avaloon.com.br>
2022-05-24 11:17:02 -0300

    Atualizando o Heading

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -2,17 +2,28 @@ import P from 'prop-types'
 import { theme } from '../../styles/theme';
 import * as Styled from "./styles";
 
-export const Heading = ({children, colorDark = true, as = 'h1', size = 'big'}) => {
+export const Heading = ({
+    children,
+    colorDark = true,
+    as = 'h1',
+    size = 'big',
+    uppercase = false,
+}) => {
     return (
-        <Styled.Title theme={theme} colorDark={colorDark} as={as} size={size}>
-            {children}
+        <Styled.Title
+        colorDark={colorDark}
+        as={as}
+        size={size}
+        uppercase={uppercase}
+        >
+        {children}
         </Styled.Title>
-    )
-}
+    );
+};
 
 Heading.protoTypes = {
-    children: P.node.isRequired,
     colorDark: P.bool.isRequired,
     as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
-    size: P.oneOf(['small', 'medium', 'big']).isRequired
+    size: P.oneOf(['small', 'medium', 'big', 'huge']).isRequired,
+    uppercase: P.bool.isRequired,
 }
--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -2,20 +2,27 @@ import styled, { css } from 'styled-components'
 
 const titleSize = {
     small: (theme) => css`
-        font-size: ${theme.font.sizes.small}
+      font-size: ${theme.font.sizes.medium};
     `,
     medium: (theme) => css`
-        font-size: ${theme.font.sizes.medium}
+      font-size: ${theme.font.sizes.large};
     `,
     big: (theme) => css`
-        font-size: ${theme.font.sizes.big}
-    `
-}
+      font-size: ${theme.font.sizes.xlarge};
+    `,
+    huge: (theme) => css`
+      font-size: ${theme.font.sizes.xhuge};
+    `,
+};
+  
+const titleCase = (uppercase) => css`
+    text-transform: ${uppercase ? 'uppercase' : 'none'};
+`;
 
 export const Title = styled.h1`
-    ${({theme, colorDark, size}) => css`
-        color: ${colorDark ? theme.colors.primaryColor : theme.colors.white };
+    ${({ theme, colorDark, size, uppercase }) => css`
+        color: ${colorDark ? theme.colors.primaryColor : theme.colors.white};
         ${titleSize[size](theme)};
-        
+        ${titleCase(uppercase)};
     `}
 `;
